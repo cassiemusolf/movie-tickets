0b37ced27d2a1f3b4ce73cc3c43d6e0614794b2f
Cassie Musolf and Kory Skarbek <kory@>
2017-01-24 12:07:14 -0800

    BOOM"

---
+++ README.md
@@ -0,0 +1,13 @@
+## Specs
+
+The program will store the users selected age
+* **Input:** Senior
+* **Output:** "senior"
+
+The program will return age appropriate movies
+* **Input:** Child
+* **Output:** Kids movies
+
+The program will take the users Movie and time input. It will then store them.
+* **Input:** The Accountant, Matinee
+* **Output:** $10.50
---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+// Business Logic
+
+function showMovies(userInputAge) {
+  if (userInputAge === 'Child (12 and under)') {
+    $('#kidsMovies, #time').show();
+  } else {
+    $('#adultMovies, #time').show();
+  }
+}
+
+function Ticket (Movie, Time, Age) {
+  this.userMovie = Movie;
+  this.userTime = Time;
+  this.userAge = Age;
+}
+
+Ticket.prototype.price = function() {
+  var defaultPrice = 10.50;
+  console.log(this.userAge);
+  console.log(this.userTime);
+  if (this.userAge === 'Child (12 and under)' && this.userTime === 'Matinee (Before 4pm)') {
+    return defaultPrice -4;
+  } else if (this.userAge === 'Child (12 and under)' && this.userTime === 'Evening') {
+    return defaultPrice -2;
+  } else if (this.userAge === 'Adult' && this.userTime === 'Matinee (Before 4pm)') {
+    return defaultPrice;
+  } else if (this.userAge === 'Adult' && this.userTime === 'Evening') {
+    return defaultPrice +2;
+  } else if (this.userAge === 'Senior (65 and up)' && this.userTime === 'Matinee (Before 4pm)') {
+    return defaultPrice -2;
+  } else if (this.userAge === 'Senior (65 and up)' && this.userTime === 'Evening') {
+    return defaultPrice;
+  }
+  console.log(defaultPrice);
+}
+
+// User Logic
+
+$(document).ready(function() {
+  var userInputAge;
+  $('form#form-age').submit(function(event) {
+    event.preventDefault();
+
+    $('.hide-me').hide();
+    $('.hide-me2').hide();
+
+    userInputAge = $('select#age').val();
+    // var ageUserInputs = new selectAge(userInputAge);
+
+    return showMovies(userInputAge);
+});
+
+  $('form.movie-times').submit(function(event) {
+    event.preventDefault();
+
+    var pgMovies = $('#pg').val();
+    var pg13Movies = $('#pg13').val();
+    var userTimeSelected = $('#timeSelected').val();
+    var moviesUserInput1 = new Ticket(pgMovies, userTimeSelected, userInputAge);
+    var moviesUserInput2 = new Ticket(pg13Movies, userTimeSelected, userInputAge);
+    $('.hide-me2').show();
+
+    $("#span").text(moviesUserInput2.price());
+  });
+});
